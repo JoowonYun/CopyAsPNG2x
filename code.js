974fe4a7d5be92439011731bf2e50a10a6d2d7d1
Joowon <iam.yunjoowon@gmail.com>
2020-08-29 12:46:47 +0900

    feat: init

---
+++ code.js
@@ -0,0 +1,47 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const MAX_SIZE = 1440;
+const imageWidth = figma.currentPage.selection[0].width;
+const imageHeight = figma.currentPage.selection[0].height;
+if (imageWidth > MAX_SIZE || imageHeight > MAX_SIZE) {
+    figma.notify("Image must be less than 1440 x 1440");
+    figma.closePlugin();
+}
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { visible: false });
+let exportSetting = {
+    format: "PNG",
+    suffix: "",
+    contentsOnly: true,
+    constraint: {
+        type: "SCALE",
+        value: 2
+    }
+};
+figma.currentPage.selection[0].exportAsync(exportSetting).then((value) => {
+    try {
+        figma.ui.postMessage({ type: 'send-img', bData: value, width: imageWidth }, { origin: "*" });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
+    if (msg === "close") {
+        setTimeout(() => {
+            figma.closePlugin();
+        }, 500);
+    }
+});
---
+++ code.ts
@@ -0,0 +1,55 @@
+const MAX_SIZE = 1440
+
+const imageWidth = figma.currentPage.selection[0].width
+const imageHeight = figma.currentPage.selection[0].height
+
+if(imageWidth > MAX_SIZE || imageHeight > MAX_SIZE) {
+  figma.notify("The image must be less than 1440*1440 pixels.")
+  figma.closePlugin()
+}
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { visible: false });
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+interface ExportSettingsImage {
+  readonly format: "JPG" | "PNG"
+  readonly contentsOnly?: boolean    // defaults to true
+  readonly suffix?: string
+  readonly constraint?: ExportSettingsConstraints
+}
+
+let exportSetting: ExportSettingsImage = {
+  format: "PNG",
+  suffix: "",
+  contentsOnly: true,
+  constraint: {
+    type: "SCALE",
+    value: 2
+  }
+}
+
+figma.currentPage.selection[0].exportAsync(exportSetting).then((value) => {
+  try {
+    figma.ui.postMessage({type: 'send-img', bData: value, width: imageWidth}, {origin: "*"})
+
+  } catch (error) {
+    console.log(error)    
+  }
+})
+
+figma.ui.onmessage = async (msg) => {
+  if(msg === "close") {
+    setTimeout(() => {
+      figma.closePlugin()
+    }, 500)
+  }
+}
